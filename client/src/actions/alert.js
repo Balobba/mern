43cad2ed823c8bdbdc4a221f0f05641cbd6ab79d
Oscar <balobba@gmail.com>
2019-08-21 21:31:41 +0200

    Implement action into alert component

--- client/src/actions/alert.js
+++ client/src/actions/alert.js
@@ -1,7 +1,8 @@
 import { SET_ALERT, REMOVE_ALERT } from './types';
 import uuid from 'uuid';
 
-export const setAlert = (msg, alertType) => dispatch => {
+// Notes: An action
+export const setAlert = (msg, alertType, timeout = 5000) => dispatch => {
   // Notes: Creates random universal ID
   const id = uuid.v4();
 
@@ -9,4 +10,6 @@ export const setAlert = (msg, alertType) => dispatch => {
     type: SET_ALERT,
     payload: { msg, alertType, id }
   });
+
+  setTimeout(() => dispatch({ type: REMOVE_ALERT, payload: id }), timeout);
 };
