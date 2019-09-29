91eed6f55644c2d9dbe0751ca2fa553ee42135ed
Oscar <balobba@gmail.com>
2019-09-29 16:21:13 +0200

    Add action and reducer to register

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,7 +1,9 @@
 import { combineReducers } from 'redux';
 import alert from './alert';
+import auth from './auth';
 
 export default combineReducers({
   //Add reducers here
-  alert
+  alert,
+  auth
 });
