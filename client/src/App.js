277350d09eb44fd34597d0d29d84a5ceb4a6476d
Magnusson Oscar <Oscar.Magnusson@etteplan.com>
2019-11-27 14:58:10 +0100

    Add profile page routes

--- client/src/App.js
+++ client/src/App.js
@@ -12,6 +12,7 @@ import CreateProfile from "./components/profile-forms/CreateProfile";
 import EditProfile from "./components/profile-forms/EditProfile";
 import AddExperience from "./components/profile-forms/AddExperience";
 import Profiles from "./components/profiles/Profiles";
+import Profile from "./components/profile/Profile";
 
 import setAuthToken from "./utils/setAuthToken";
 // Redux
@@ -43,6 +44,8 @@ const App = () => {
               <Route exact path="/register" component={Register} />
               <Route exact path="/login" component={Login} />
               <Route exact path="/profiles" component={Profiles} />
+              <Route exact path="/profile/:id" component={Profile} />
+
               <PrivateRoute exact path="/dashboard" component={Dashboard} />
               <PrivateRoute
                 exact
