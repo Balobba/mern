f9f651cdc1155e8e6f15db30eeb48ba53c5c8957
Magnusson Oscar <Oscar.Magnusson@etteplan.com>
2019-11-28 13:30:37 +0100

    Add reducer for likes. Bugfix

--- client/src/reducers/post.js
+++ client/src/reducers/post.js
@@ -1,4 +1,4 @@
-import { GET_POSTS, POST_ERROR } from "../actions/types";
+import { GET_POSTS, POST_ERROR, UPDATE_LIKES } from "../actions/types";
 
 const initialState = {
   posts: [],
@@ -18,10 +18,20 @@ export default function(state = initialState, action) {
         loading: false
       };
 
-    case GET_POSTS:
+    case POST_ERROR:
       return {
         ...state,
         error: payload,
+        loading: false
+      };
+
+    case UPDATE_LIKES:
+      return {
+        ...state,
+        // Notes: Check to see if post ID matches the payload post ID before manipulating likes
+        posts: state.posts.map(post =>
+          post._id === payload.id ? { ...post, likes: payload.likes } : post
+        ),
         loading: false
       };
 
