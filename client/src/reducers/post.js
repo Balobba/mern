470a567a90ec7cd210e04d24c65d90fc9e9ab137
Magnusson Oscar <Oscar.Magnusson@etteplan.com>
2019-11-29 13:44:25 +0100

    Bugfixes. Removed unused stuff

--- client/src/reducers/post.js
+++ client/src/reducers/post.js
@@ -20,17 +20,17 @@ export default function(state = initialState, action) {
   const { type, payload } = action;
 
   switch (type) {
-    case GET_POSTS:
-      return {
-        ...state,
-        posts: payload,
-        loading: false
-      };
-
     case GET_POST:
       return {
         ...state,
         post: payload,
+        loading: false
+      };
+
+    case GET_POSTS:
+      return {
+        ...state,
+        posts: payload,
         loading: false
       };
 
