faa307e8f7ad7b9fb1a354e3c3c53d267576a777
Magnusson Oscar <Oscar.Magnusson@etteplan.com>
2019-11-29 10:49:06 +0100

    Add comment section. Add delete functionality

--- client/src/components/post/Post.js
+++ client/src/components/post/Post.js
@@ -7,6 +7,7 @@ import { Link } from "react-router-dom";
 
 import { getPost } from "../../actions/post";
 import CommentForm from "./CommentForm";
+import CommentItem from "./CommentItem";
 
 const Post = ({ getPost, post: { post, loading }, match }) => {
   useEffect(() => {
@@ -25,6 +26,11 @@ const Post = ({ getPost, post: { post, loading }, match }) => {
       of Redux actions and states as props to a component */}
       <PostItem post={post} showActions={false} />
       <CommentForm postId={post._id} />
+      <div className="comments">
+        {post.comments.map(comment => (
+          <CommentItem key={comment._id} comment={comment} postId={post._id} />
+        ))}
+      </div>
     </Fragment>
   );
 };
